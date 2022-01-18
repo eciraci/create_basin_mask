25007bc3d1b8ab3ad45e8f5c8311c6f0d0ad3fc4
Enrico Ciraci <eciraci@uci.edu>
2022-01-17 19:31:34 -0800

    github workflow to test python environment added.

--- test.sh
+++ test.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-python convert_shp_to_raster.py './data/input/Indus.dir/Indus.shp'
+#python convert_shp_to_raster.py './data/input/Indus.dir/Indus.shp' --f_type='tiff' --res=0.5
+python convert_shp_to_raster.py './data/input/Indus.dir/Indus.shp'  --res=0.5
